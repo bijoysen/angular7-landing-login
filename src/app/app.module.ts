0f24aca7af44f458420d8b3b82cb52702dde4f83
bijoysen <bjoysen@gmail.com>
2018-12-14 11:51:50 +0530

    Structure updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { HomeComponent } from './components/home/home.component';
-import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    HomeComponent,
-    SignupComponent
+    AppComponent
   ],
   imports: [
-    CommonModule,
     BrowserModule,
-    FormsModule,
-    RouterModule,
     AppRoutingModule
   ],
   providers: [],
