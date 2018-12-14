31eb4f7a2a44df301e604e44df5b56e248e43aed
bijoysen <bjoysen@gmail.com>
2018-12-14 18:36:54 +0530

    Header, footer & Login component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { LandingComponent } from './pre/landing/landing.component';
+import { LoginComponent } from './pre/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
