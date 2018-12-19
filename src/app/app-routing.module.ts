b92aa8abd98826be579213196e0338c89db16854
sguha-work <sguha1988.life@gmail.com>
2018-12-19 23:23:01 +0530

    page link created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,33 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pre/home/home.component';
 import { FaqComponent } from './pre/faq/faq.component';
 import { ReviewComponent } from './pre/review/review.component';
+import { LoginOneComponent } from './pre/login-one/login-one.component';
+import { LoginTwoComponent } from './pre/login-two/login-two.component';
+import { LoginThreeComponent } from './pre/login-three/login-three.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    redirectTo: '/home/login1',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'login1',
+        component: LoginOneComponent
+      },
+      {
+        path: 'login2',
+        component: LoginTwoComponent
+      },
+      {
+        path: 'login3',
+        component: LoginThreeComponent
+      }
+    ]
   },
   {
     path: 'faq',
